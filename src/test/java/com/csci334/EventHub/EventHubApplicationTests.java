e4af56e954f7db012482ab73bd404ede70ede740
khanhsaan <109020045+khanhsaan@users.noreply.github.com>
2025-06-06 22:36:17 +1000

    modify

--- src/test/java/com/csci334/EventHub/EventHubApplicationTests.java
+++ src/test/java/com/csci334/EventHub/EventHubApplicationTests.java
@@ -3,9 +3,18 @@ package com.csci334.EventHub;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
+@TestPropertySource(properties = {
+		"spring.datasource.url=jdbc:h2:mem:testdb;MODE=MySQL;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE;NON_KEYWORDS=USER",
+		"spring.datasource.driver-class-name=org.h2.Driver",
+		"spring.datasource.username=sa",
+		"spring.datasource.password=",
+		"spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect",
+		"spring.jpa.hibernate.ddl-auto=create-drop"
+})
 class EventHubApplicationTests {
 
 	@Test
